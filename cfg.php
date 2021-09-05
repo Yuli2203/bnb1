08f4f05dc605925ac4b9c01eefb7724f5b7cd2af
Yuli2203 <90106859+Yuli2203@users.noreply.github.com>
2021-09-05 07:14:07 +0700

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,23 @@
+<?php
+
+
+
+
+$ua = array(
+"Host:freebnb.cc",
+
+"accept:application/json, text/javascript, */*; q=0.01",
+
+"origin:https://freebnb.cc",
+
+"user-agent:Mozilla/5.0 (Linux; Android 6.0.1; vivo 1606) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.52 Mobile Safari/537.36",
+
+"content-type:application/x-www-form-urlencoded; charset=UTF-8",
+
+"referer:https:https://freebnb.cc/?r=82549",
+
+"cookie:PHPSESSID=gqps5i6hak1qdkdov4c6qtsv20;referral=82549"
+);
+
+
+//script by Biki_Scripter
